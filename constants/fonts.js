7ad924ed5982ee47fccfcbc91dece3e55fa2431b
jailbreakerVC <54243183+jailbreakerVC@users.noreply.github.com>
2023-06-28 14:44:27 +0530

    added style sheets (remove these later)

---
+++ constants/fonts.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const FONTS = StyleSheet.create({
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+});
+
+export default FONTS;
---
+++ constants/styles.js
@@ -0,0 +1,13 @@
+const COLORS = {
+  primary_light: "",
+  primary_dark: "#9030f0",
+  secondary_light: "",
+  secondary_dark: "#4f4a54",
+
+  text_dark: "#ece9f0",
+  text_light: "#000000",
+
+  background_dark: "#2a292b",
+};
+
+export { COLORS };
