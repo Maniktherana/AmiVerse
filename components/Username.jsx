db1593caee75659240dde7d015b9353c18dc50aa
VIJIT CHANDNA <vijit123chandna@gmail.com>
2023-07-10 11:46:06 +0530

    created timetable component and functions folder

--- components/Username.jsx
+++ components/Username.jsx
@@ -1,47 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
-import axios from "axios";
-import { FONTS } from "../constants/fonts";
-import { PaperProvider, Text } from "react-native-paper";
-// import Config from "react-native-config";
-
-async function getStudentProfile() {
-  let baseUrl = "http://localhost:3000/api/v1/user_profile";
-  let username = "";
-  let password = "";
-
-  let response = await axios.post(
-    baseUrl,
-    {},
-    {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,PUT,POST",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      },
-      auth: {
-        username,
-        password,
-      },
-    }
-  );
-  return response.data;
-}
+import { View, Text } from "react-native";
+import { getStudentProfile } from "../controllers/functions";
 
 function Username() {
-  const [data, setData] = useState({});
+  const [userid, setUserid] = useState({});
+ 
 
   useEffect(() => {
-    getStudentProfile().then((data) => setData(data));
+    getStudentProfile().then((data) => setUserid(data));
   }, []);
 
+
   return (
-    // <PaperProvider>
+    <>
     <View>
-      <Text>Hello {data.name}</Text>
+      <Text>Hello {userid.name}</Text>
     </View>
-    // </PaperProvider>
+    </>
   );
 }
 
