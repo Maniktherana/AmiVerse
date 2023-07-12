769d44b0cc77682aeb7ddd8f207a97b02e3c3091
Manik Rana <manikrana54@gmail.com>
2023-07-12 19:52:31 +0530

    Revert "Merge pull request #7 from Maniktherana/add-missing-files"
    This reverts commit 4dd89979c5bf420cb18008efe1e84017047636df, reversing
    changes made to 40c7aa616821e9807fb8ab3e78c7ea706921eb2f.

--- apps/app/index.js
+++
@@ -1,6 +0,0 @@
-// AppRegistry.registerComponent(appName, () => Main);
-import { Redirect } from "expo-router";
-
-export default function Index() {
-  return <Redirect href="/home" />;
-}
--- apps/index.js
+++ apps/index.js
@@ -1,4 +1,4 @@
-//the following code is required to resolve the "btoa() error on android"
+// the following code is required to resolve the "btoa() error on android"
 import { decode, encode } from "base-64";
 
 if (!global.btoa) {
